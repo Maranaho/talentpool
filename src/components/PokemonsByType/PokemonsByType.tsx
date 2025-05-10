a91d78eb7c97f26a9095ce6694e6906a67b32b29
Maranaho N'Guessan <maranahonguessan@Maranahos-MacBook-Pro.local>
2025-05-10 22:06:44 +0100

    listing pokemon by types good:)

--- src/components/PokemonsByType/PokemonsByType.tsx
+++ src/components/PokemonsByType/PokemonsByType.tsx
@@ -1,14 +1,26 @@
 import type { FC } from "react"
+import { usePokemonByType } from "../../hooks/usePokemonByType"
 interface PokemonsByTypeProps {
   pokemonType: string
 }
 
 export const PokemonsByType:FC<PokemonsByTypeProps> = ({ pokemonType }) => {
+  const { loading , error , pokemons } = usePokemonByType(pokemonType)
+
+  if(error) return <p>{error}</p>
+  if(loading) return <p>Loading...</p>
 
   return (
     <section>
       <h1>PokemonsByType</h1>
-      <span>{pokemonType}</span>
+      {pokemons.map(pokemon=>(
+        <button
+          key={pokemon}
+        >
+          <img src={`https://play.pokemonshowdown.com/sprites/ani/${pokemon}.gif`} />
+          <span>{pokemon}</span>
+        </button>
+      ))}
     </section>
   )
 }
