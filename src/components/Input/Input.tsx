b95a93e9913802fcf7202510291d4c62331e10ab
Maranaho N'Guessan <maranahonguessan@Maranahos-MacBook-Pro.local>
2025-05-31 23:11:08 -0700

    removed unused useState

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import  { useState, type FC, type ChangeEventHandler } from "react"
+import  { type FC, type ChangeEventHandler } from "react"
 import styles from "./Input.module.scss"
 export type Value = string | number | undefined
 export type OnChange = ChangeEventHandler<HTMLInputElement>
